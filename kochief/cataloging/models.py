18f884bc4d5df03bac286860c44619fafa119a2d
gsf747 <none@none>
2009-04-16 15:35:37 +0000

    moved models and fixtures from cataloging to datastore.  refs #2
    --HG--
    extra : convert_revision : svn%3A8a3318d3-b46f-40e0-9460-50b0ffd28b6e/trunk%40374

--- kochief/cataloging/models.py
+++ kochief/cataloging/models.py
@@ -1,4 +1,4 @@
-# Copyright 2008 Gabriel Farrell
+# Copyright 2009 Gabriel Farrell
 #
 # This file is part of Kochief.
 # 
@@ -15,43 +15,4 @@
 # You should have received a copy of the GNU General Public License
 # along with Kochief.  If not, see <http://www.gnu.org/licenses/>.
 
-from django.contrib import admin
-from django.contrib.auth.models import User
-from django.db import models
-from django.utils import simplejson
-
-class Record(models.Model):
-    def __unicode__(self):
-        return unicode(self.id)
-
-    def get_current(self):
-        return self.get_versions()[0]
-
-    def get_versions(self):
-        return self.version_set.order_by('-id')
-
-class Version(models.Model):
-    record = models.ForeignKey(Record)
-    data = models.TextField()
-    timestamp = models.DateTimeField(auto_now_add=True)
-    message = models.CharField(max_length=256)
-    committer = models.ForeignKey(User)
-
-    class Meta:
-        ordering = ['-timestamp']
-
-    def __unicode__(self):
-        return self.message
-
-    def get_data(self):
-        return simplejson.loads(self.data)
-
-class VersionInline(admin.TabularInline):
-    model = Version
-    extra = 1
-
-class RecordAdmin(admin.ModelAdmin):
-    inlines = [VersionInline]
-
-admin.site.register(Record, RecordAdmin)
-admin.site.register(Version)
+"""Models for the connection (via Jangle) between cataloging and datastore."""
